dc22f6260f4f1059802f367899d7a5981931fd4b
Nammy1101 <nammy1101@hotmail.com>
2013-09-28 12:34:02 -0600

    index.php

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
+<<<<<<< HEAD
 <?php
 ?>
 <html>
 <html>
+=======
+<!DOCTYPE html>
+<head>
+
+	<title>GitClub</title>
+
+</head>
+
+<h1> Welcome to the GitClub </h1>
+
+<form name="login" action="index.php" method="post">
+
+	UserName: <input type="text" name="username"> <br>
+	Password: <input type="password" name="password">
+	<input type="submit" value="submit">
+
+</form>
+
+	
+
+</html>
+>>>>>>> index.php
