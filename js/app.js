2564ddb6730a6af7c14001b221f426f3c0a10341
root <root@david-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-09-28 13:33:16 -0600

    Added js libs

---
+++ js/app.js
@@ -0,0 +1,81 @@
+﻿ var UsersView = Backbone.View.extend({
+        tagName: "article",
+        className: "user-container",
+        template: _.template($("#userTemplate").html())
+
+        render: function () {
+            this.$el.html(this.template(this.model.toJSON()));
+            return this;
+        },
+
+        events: {
+        },
+
+        //delete a contact
+        deleteContact: function () {
+        },
+
+        //switch contact to edit mode
+        editContact: function () {
+            this.$el.html(this.editTemplate(this.model.toJSON()));
+
+            //add select to set type
+            var newOpt = $("<option/>", {
+                html: "<em>Add new...</em>",
+                value: "addType"
+            });
+
+            this.select = directory.createSelect().addClass("type").val(this.$el.find("#type").val()).append(newOpt).insertAfter(this.$el.find(".name"));
+            this.$el.find("input[type='hidden']").remove();
+        },
+
+        addType: function () {
+            if (this.select.val() === "addType") {
+
+                this.select.remove();
+
+                $("<input />", {
+                    "class": "type"
+                }).insertAfter(this.$el.find(".name")).focus();
+            }
+        },
+
+        saveEdits: function (e) {
+            e.preventDefault();
+
+            var formData = {},
+                prev = this.model.previousAttributes();
+
+            //get form data
+            $(e.target).closest("form").find(":input").not("button").each(function () {
+                var el = $(this);
+                formData[el.attr("class")] = el.val();
+            });
+
+            //use default photo if none supplied
+            if (formData.photo === "") {
+                delete formData.photo;
+            }
+
+            //update model
+            this.model.set(formData);
+
+            //render view
+            this.render();
+
+            //if model acquired default photo property, remove it
+            if (prev.photo === "/img/placeholder.png") {
+                delete prev.photo;
+            }
+
+            //update contacts array
+            _.each(contacts, function (contact) {
+                if (_.isEqual(contact, prev)) {
+                    contacts.splice(_.indexOf(contacts, contact), 1, formData);
+                }
+            });
+        },
+
+        cancelEdit: function () {
+        }
+    });
