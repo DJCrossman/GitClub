b6fe99d2f991c6b98e226bf2180ca6c633202940
warribbons <nicholas.phongsavath@gmail.com>
2013-09-28 10:04:01 -0600

    test.js

---
+++ test.js
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function displayDate()
+{
+document.getElementById("demo").innerHTML=Date();
+}
+</script>
+</head>
+<body>
+
+<h1>My First JavaScript</h1>
+<p id="demo">This is a paragraph.</p>
+
+<button type="button" onclick="displayDate()">Display Date</button>
+
+</body>
+</html> 
