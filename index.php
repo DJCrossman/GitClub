5dffe620fe30b3e8620b7627503ebb3e219c029b
root <root@david-HP-Pavilion-dv6-Notebook-PC.(none)>
2013-09-28 13:04:53 -0600

    Modified index.php

--- index.php
+++ index.php
@@ -1,27 +1,117 @@
-<<<<<<< HEAD
 <?php
 ?>
-<html>
-<html>
-=======
 <!DOCTYPE html>
-<head>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <title>GitClub</title>
+        <link rel="stylesheet" href="https://github.global.ssl.fastly.net/assets/github-4288f026700410ae032b5d324dea2b4571789d7c.css" />
+        <link rel="stylesheet" href="https://github.global.ssl.fastly.net/assets/github2-88d5087029dbe346f413843c4cb0149921840ef5.css" />
+    </head>
+    <body>
+    	<header>
+    		<div class="header header-logged-in true">
+  <div class="container clearfix">
 
-	<title>GitClub</title>
+    <a class="header-logo-invertocat" href="https://github.com/">
+  <span class="mega-octicon octicon-mark-github"></span>
+</a>
 
-</head>
+    
+    <a href="/notifications" class="notification-indicator tooltipped downwards" data-gotokey="n" original-title="You have unread notifications">
+        <span class="mail-status unread"></span>
+</a>
 
-<h1> Welcome to the GitClub </h1>
+      <div class="command-bar js-command-bar  ">
+          <form accept-charset="UTF-8" action="/search" class="command-bar-form" id="top_search_form" method="get">
 
-<form name="login" action="index.php" method="post">
+<input type="text" data-hotkey="/ s" name="q" id="js-command-bar-field" placeholder="Search or type a command" tabindex="1" autocapitalize="off" data-username="DJCrossman">
 
-	UserName: <input type="text" name="username"> <br>
-	Password: <input type="password" name="password">
-	<input type="submit" value="submit">
+
+  <span class="octicon help tooltipped downwards" original-title="Show command bar help">
+    <span class="octicon octicon-question"></span>
+  </span>
+
+
+  <input type="hidden" name="ref" value="cmdform">
 
 </form>
+        <ul class="top-nav">
+          <li class="explore"><a href="/explore">Explore</a></li>
+            <li><a href="https://gist.github.com">Gist</a></li>
+            <li><a href="/blog">Blog</a></li>
+          <li><a href="https://help.github.com">Help</a></li>
+        </ul>
+      </div>
 
-	
+    
 
+
+  <ul id="user-links">
+    <li>
+      <a href="/DJCrossman" class="name">
+        <img height="20" src="https://1.gravatar.com/avatar/bb17cafc08254c48919d12213f117e8e?d=https%3A%2F%2Fidenticons.github.com%2Ff5852fd0e3770196c2fa0054caa9386e.png&amp;s=140" width="20"> DJCrossman
+      </a>
+    </li>
+
+      <li>
+        <a href="/new" id="new_repo" class="tooltipped downwards" aria-label="Create a new repo" original-title="Create a new repo">
+          <span class="octicon octicon-repo-create"></span>
+        </a>
+      </li>
+
+      <li>
+        <a href="/settings/profile" id="account_settings" class="tooltipped downwards" aria-label="Account settings " original-title="Account settings ">
+          <span class="octicon octicon-tools"></span>
+        </a>
+      </li>
+      <li>
+        <a class="tooltipped downwards" href="/logout" data-method="post" id="logout" aria-label="Sign out" original-title="Sign out">
+          <span class="octicon octicon-log-out"></span>
+        </a>
+      </li>
+
+  </ul>
+
+<div class="js-new-dropdown-contents hidden">
+  
+
+<ul class="dropdown-menu">
+  <li>
+    <a href="/new"><span class="octicon octicon-repo-create"></span> New repository</a>
+  </li>
+  <li>
+    <a href="/organizations/new"><span class="octicon octicon-organization"></span> New organization</a>
+  </li>
+
+
+
+</ul>
+
+</div>
+
+
+    
+  </div>
+</div>
+    	</header>
+    	<article>
+    	</article>
+        <div id="contacts"></div>
+	<script id="contactTemplate" type="text/template">
+    	  <img src="<%= photo %>" alt="<%= name %>" />
+    	  <h1><%= name %><span><%= type %></span></h1>
+    	  <div><%= address %></div>
+    	  <dl>
+        	<dt>Tel:</dt><dd><%= tel %></dd>
+        	<dt>Email:</dt><dd><a href="mailto:<%= email %>"><%= email %></a></dd>
+	  </dl>
+	</script>
+        <script src="js/jquery-1.7.1.min.js"></script>
+	<script src="js/json2.js"></script>
+        <script src="js/Underscore-min.js"></script>
+        <script src="js/Backbone-min.js"></script>
+        <script src="js/app.js"></script>
+    </body>
 </html>
->>>>>>> index.php
+</html>
